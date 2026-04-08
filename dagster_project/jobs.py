4842d64fe40dd55e61d8e4fcca3f9efeb1504940
Khushboo Padmakar <khushboopadmakar@gmail.com>
2026-04-08 17:19:20 +0530

    Update jobs and fix CI workflow

--- dagster_project/jobs.py
+++ dagster_project/jobs.py
@@ -1,10 +1,11 @@
 from dagster import job
-from .ops import run_extract, run_transform, run_load, run_dbt,run_tests
+from .ops import run_extract,run_tests, run_transform, run_load, run_dbt
 
 @job
 def etl_pipeline():
     extract = run_extract()
+    run_tests()
     transform = run_transform()
     load = run_load()
     dbt_result = run_dbt()
-    run_tests()
+    
